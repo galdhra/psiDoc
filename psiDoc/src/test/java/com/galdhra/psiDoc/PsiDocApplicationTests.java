91c6ac74d2b21ab0c5ab3a772387b31d29526638
Luiz Fernando Azevedo <luiz1337@gmail.com>
2025-05-29 11:27:35 -0300

    project

---
+++ psiDoc/src/test/java/com/galdhra/psiDoc/PsiDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galdhra.psiDoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiDocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
